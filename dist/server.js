faaeb66f58005937085d3767a14380eab75647e2
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-20 11:30:46 -0700

    feat: Initial Setup

---
+++ dist/server.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("EXUCUTED!");
+const person = "Sam";
+const count = 100;
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("EXUCUTED!");
+
+import moment from 'moment';
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+const person: string = "Sam";
+const count: number = 100;
