7218faeaeeb81225ef9dad1d41d1decb17704fc4
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-21 11:15:35 -0700

    feat: completed MITASK M

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,29 @@
 //============================================================
+                                                    // MITASK M
+
+
+function getSquareNumbers(arr: number[]) {
+    return arr.map(number => ({
+        number,
+        square: number * number
+    }));
+}
+
+console.log(getSquareNumbers([3, 2, 6]));
+console.log(getSquareNumbers([-3, -2, 0]));
+
+//============================================================
                                                     //MITASK K
 
 
-function reverseSentence(sentence: string): string {
-    return sentence
-        .split(" ")
-        .map(word => word.split("").reverse().join(""))
-        .join(" ");
-}
+// function reverseSentence(sentence: string): string {
+//     return sentence
+//         .split(" ")
+//         .map(word => word.split("").reverse().join(""))
+//         .join(" ");
+// }
 
-console.log(reverseSentence("we like coding!"));
-console.log(reverseSentence("My name is Sam, currently I am working in B2C"));
-console.log(reverseSentence("HEH"));
+// console.log(reverseSentence("we like coding!"));
+// console.log(reverseSentence("My name is Sam, currently I am working in B2C"));
+// console.log(reverseSentence("HEH"));
 // console.log("Hello World!");
