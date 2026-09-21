6c4a0e3a256eedbd0a610c6ca0a706a9c6b3daef
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-20 23:01:38 -0700

    feat: complete MITASK K

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+//============================================================
+                                                    //MITASK K
+
+
+function reverseSentence(sentence: string): string {
+    return sentence
+        .split(" ")
+        .map(word => word.split("").reverse().join(""))
+        .join(" ");
+}
+
+console.log(reverseSentence("we like coding!"));
+console.log(reverseSentence("My name is Sam, currently I am working in B2C"));
+console.log(reverseSentence("HEH"));
+// console.log("Hello World!");
