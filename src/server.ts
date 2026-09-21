3a3d5c8add21734cbcde1c876d1adf26ddf2b291
Samandar(Sam) Turdaliev <abc@abcs-MacBook-Air.local>
2026-09-20 22:51:27 -0700

    feat: Integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,2 @@
-console.log("EXUCUTED!");
-
-import moment from 'moment';
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-const person: string = "Sam";
-const count: number = 100;
+import dotenv from 'dotenv';
+dotenv.config();
